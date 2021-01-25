07e1b26993559da7fcf445062929ce21d231722e
dayana-sog <dayanasog23@gmail.com>
2021-01-25 17:34:37 +0000

    configurações iniciais

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { FooterWrapper } from './styles';
+
+const Footer= (props) => {
+  return (
+    <FooterWrapper {...props}>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante
+        {' '}
+        a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          <span>Imersão React da Alura</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  );
+}
+
+export default Footer;
